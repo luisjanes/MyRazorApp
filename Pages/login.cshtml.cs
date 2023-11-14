fc7163491c5e4b238928c4200a40c95925c74b2a
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-11-14 11:26:27 -0300

    feat: add login page

---
+++ Pages/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
